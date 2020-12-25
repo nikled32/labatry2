bd3f64ceda17c5173e4219b6a06b8546e518993b
Nikita Ledovskii <nikled32@outlook.com>
2020-12-25 16:55:53 +0300

    Добавьте файлы проекта.

---
+++ laba8git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laba8git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Знакомство с GITHUB");
+        }
+    }
+}
